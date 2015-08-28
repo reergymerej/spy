78dcaaeb56238c1fadb34b9a030cbccc2c3532ef
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-08-27 22:52:51 -0400

    infiltrate/exfiltrate

--- test/app.js
+++ test/app.js
@@ -1,89 +1,102 @@
 'use strict';
 
 var will = require('willy').will;
-var app = require('../app.js');
+var app = require('../bin/app.js');
 
-describe('sanity', function () {
-  var obj;
+describe('method', function () {
+  var foo;
+  var count;
 
   beforeEach(function () {
-    obj = {
-      foo: function () {},
-      bar: function () {}
+    foo = {
+      bar: function () {
+        count++;
+      }
     };
+
+    count = 0;
   });
 
-  it('should keep track of each call', function () {
-    app(obj, 'foo');
-    obj.foo();
+  describe('infiltrate', function () {
+    it('should track calls', function () {
+      var args = [1, 2, 3, 4];
+      app.infiltrate(foo, 'bar');
+      foo.bar.apply(foo.bar, args);
 
-    will(obj.foo.calls.length).be(1);
-  });
-
-  it('should work with functions', function () {
-    var fn = app(function () {});
-
-    fn();
-    fn();
-    fn();
-    will(fn.calls.length).be(3);
-  });
-
-  it('should keep track of each call and the arguments passed', function () {
-    app(obj, 'foo');
-    obj.foo(1, 2, 3);
-    obj.foo(4);
-    obj.foo(5, 6, 7, 8, 9);
-
-    will(obj.foo.calls[3]).haveOnly(5, 6, 7, 8, 9);
-  });
-
-  describe('original function', function () {
-    it('should execute the original function', function () {
-      var called = false;
-      var fn = app(function () {
-        called = true;
-      });
-
-      fn();
-      will(called).be(true);
+      will(foo.bar.calls[0]).haveOnly(args);
     });
 
-    it('should maintain scope', function () {
-      var scope;
-      var obj = {};
-      var fn = app(function () {
-        scope = this;
-      });
+    it('should still fire the original', function () {
+      app.infiltrate(foo, 'bar');
+      foo.bar();
 
-      fn.apply(obj);
-      will(scope).be(obj);
+      will(count).be(1);
     });
   });
 
-  describe('original method', function () {
-    it('should execute the original method', function () {
-      var called = false;
-      var obj = {
-        foo: function () {
-          called = true;
-        }
-      };
+  describe('exfiltrate', function () {
+    it('should restore original method', function () {
+      var original = foo.bar.toString();
+      app.infiltrate(foo, 'bar');
+      app.exfiltrate(foo, 'bar');
 
-      obj.foo();
-      will(called).be(true);
+      will(foo.bar.toString()).be(original);
     });
 
-    it('should maintain scope', function () {
-      var scope;
-      var obj = {
-        foo: function () {
-          scope = this;
-        }
-      };
+    it('should function as before', function () {
+      app.infiltrate(foo, 'bar');
+      app.exfiltrate(foo, 'bar');
+      foo.bar();
 
-      obj.foo();
-      will(scope).be(obj);
+      will(count).be(1);
+    });
+  });
+});
+
+describe('function', function () {
+  var foo;
+  var count;
+
+  beforeEach(function () {
+    foo = function () {
+        count++;
+    };
+
+    count = 0;
+  });
+
+  describe('infiltrate', function () {
+    it('should track calls', function () {
+      var args = [1, 2, 3, 4];
+      foo =  app.infiltrate(foo);
+      foo.apply(foo, args);
+
+      will(foo.calls[0]).haveOnly(args);
+    });
+
+    it('should still fire the original', function () {
+      foo = app.infiltrate(foo);
+      foo();
+
+      will(count).be(1);
+    });
+  });
+
+  describe('exfiltrate', function () {
+    it('should restore original function', function () {
+      var original = foo.toString();
+      foo = app.infiltrate(foo);
+      foo = app.exfiltrate(foo, 'bar');
+
+      will(foo.toString()).be(original);
+    });
+
+    it('should function as before', function () {
+      foo = app.infiltrate(foo);
+      foo = app.exfiltrate(foo, 'bar');
+      foo();
+
+      will(count).be(1);
     });
   });
 });
